7b33f54ecc8ac90d653a39d7db645225fee9356f
Jay Marcyes <jay@marcyes.com>
2018-11-16 01:08:51 -0700

    removes py2 urlparse import

--- plain/parsers/__init__.py
+++ plain/parsers/__init__.py
@@ -2,7 +2,6 @@
 from __future__ import unicode_literals, division, print_function, absolute_import
 import re
 from collections import defaultdict
-from urlparse import urlparse
 import os
 import json
 
